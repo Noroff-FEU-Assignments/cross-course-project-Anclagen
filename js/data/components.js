9c0b4fe64880288036b7bc6854c1d73974f570ef
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-03-10 23:31:00 +0100

    Update components.js

--- js/data/components.js
+++ js/data/components.js
@@ -1,5 +1,5 @@
 export function checkCart(){
-  if(localStorage.cart !== null || localStorage.cart.length === 0){
+  if(localStorage.cart !== null || localStorage.cart.length !== 0){
     document.querySelector("#cart").innerHTML = `<img src="images/interface_and_logo/full_cart.png" alt="Full Cart" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`
   } else {
     document.querySelector("#cart").innerHTML = `<img src="images/interface_and_logo/empty_cart_icon_1@2x.png" alt="Cart Empty" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`
