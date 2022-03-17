1ef21fe291d52fd9e7005961dde47fdf679fee18
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-03-18 00:31:29 +0100

    contact page validation

--- js/contact.js
+++ js/contact.js
@@ -1,50 +1,24 @@
+import {checkCart, validateEmailInput, validatedInputLength, validatedNumberInputLength} from "./data/components.js"
 
+checkCart();
+
+// defined page containers
 const contactForm = document.querySelector("#contact-form");
 const fullname = document.querySelector("#fullname");
-const fullnameError = document.querySelector(".error-name");
+const fullnameError = document.querySelector("#error-name");
 const email = document.querySelector("#email");
-const emailError = document.querySelector(".error-email");
+const emailError = document.querySelector("#error-email");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#error-subject");
 const message = document.querySelector("#message");
-const messageError = document.querySelector(".error-message");
+const messageError = document.querySelector("#error-message");
 const orderNumber = document.querySelector("#order-number");
-const orderNumberError = document.querySelector(".order-number-message");
+const orderNumberError = document.querySelector("#error-order-number");
 const successContainer = document.querySelector(".success");
 
-//check length without spaces, and generate error if validation fails
-function validatedInputLength(input, length) {
-  if (input.value.trim().length > length) {
-    // error.innerHTML = "";
-    input.style.border ="2px solid green";
-    return true;
-  } else {
-    // error.innerHTML = `Your ${input.name} must have a minium of ${length + 1} characters.`;
-    input.style.border ="2px solid red";
-  }
-}
-
-function validatedNumberInputLength(input, length){
-  const numberRegEx = /^\d+$/;
-  const validInput = numberRegEx.test(input.value);
-  if(validInput && input.value.trim().length === length){
-    input.style.border ="2px solid green";
-    return true;
-  } else {
-    input.style.border ="2px solid red";
-  }
-}
-
-//validate email, generate errors if validation fails, novalidated added to form, email input cause problems with the js
-function validateEmailInput(email) {
-  const emailRegEx = /^([a-z0-9_\.\+-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-  const validateEmail = emailRegEx.test(email.value);
-  if (validateEmail){ 
-    // emailError.innerHTML = "";
-    email.style.border ="2px solid green";
-    return true;
-  } else {
-    // emailError.innerHTML = `Please enter a valid email address`;
-    email.style.border ="2px solid red";
-  }
+// return grey borders on form submission
+function resetBorders(input){
+  input.style.border = "2px solid grey"
 }
 
 //validate form
@@ -56,18 +30,29 @@ function validateContactForm(submission){
   successContainer.classList.remove("main-item");
 
   //variables assigned true if they pass, and errors generated on fail.
-  const a = validatedInputLength(fullname, 0);
-  const b = validatedInputLength(message, 29);
-  const c = validateEmailInput(email);
-  const d = validatedNumberInputLength(orderNumber, 12);
+  const a = validatedInputLength(fullname, 0, fullnameError);
+  const b = validatedInputLength(message, 29, messageError);
+  const c = validateEmailInput(email, emailError);
+  const d = validatedInputLength(subject, 5, subjectError);
+  let e = true;
+  orderNumberError.innerText = "";
+  if(orderNumber.value.length > 0){
+    e = validatedNumberInputLength(orderNumber, 12, orderNumberError);
+  }
 
   //if all variables true form submitted and success div displayed 
-  if(a && b && c && d) {
+  if(a && b && c && d && e) {
     successContainer.classList.add("main-item");
     successContainer.innerHTML = "<p>Success, your query has been submitted.</p>"
     //scrolls to success container on submit.
+
     successContainer.scrollIntoView();
     contactForm.reset();
+    resetBorders(fullname);
+    resetBorders(message);
+    resetBorders(email);
+    resetBorders(subject);
+    resetBorders(orderNumber);
   }
 }
 
--- js/data/components.js
+++ js/data/components.js
@@ -1,15 +1,58 @@
+
+// updates cart status on pages
 export function checkCart(){
-  const cart = document.querySelector("#cart");
+  const cartContainer = document.querySelector("#cart");
+
+  //checks if cart exists in local storage, error on parsing undefined on phone/tablet
   if(localStorage.cart === undefined){
-    cart.innerHTML = `<img src="images/interface_and_logo/empty_cart_icon_1@2x.png" alt="Cart Empty" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
+    cartContainer.innerHTML = `<img src="images/interface_and_logo/empty_cart_icon_1@2x.png" alt="Cart Empty" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
   } else {
     const cart = JSON.parse(localStorage.cart);
-    console.log(cart);
     if(localStorage.cart !== null && cart.length !== 0){
-      cart.innerHTML = `<img src="images/interface_and_logo/full_cart.png" alt="Full Cart" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
+      cartContainer.innerHTML = `<img src="images/interface_and_logo/full_cart.png" alt="Full Cart" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
     } else {
-      cart.innerHTML = `<img src="images/interface_and_logo/empty_cart_icon_1@2x.png" alt="Cart Empty" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
+      cartContainer.innerHTML = `<img src="images/interface_and_logo/empty_cart_icon_1@2x.png" alt="Cart Empty" class="navigation-icons" /> <span class="desktop-nav">Cart</span>`;
     }
   }
 
+}
+
+//validates text inputs
+export function validatedInputLength(input, length, errorContainer) {
+  if (input.value.trim().length > length) {
+    errorContainer.innerText = "";
+    input.style.border ="2px solid green";
+    return true;
+  } else {
+    input.style.border ="2px solid red";
+    errorContainer.innerText = `Your ${input.name} must have a minimum of ${length + 1} characters.`;
+  }
+}
+
+//validates number only inputs
+export function validatedNumberInputLength(input, length, errorContainer){
+  const numberRegEx = /^\d+$/;
+  const validInput = numberRegEx.test(input.value);
+  if(validInput && input.value.trim().length === length){
+    errorContainer.innerText = "";
+    input.style.border ="2px solid green";
+    return true;
+  } else {
+    errorContainer.innerText = "Please enter a 12 digit order number."
+    input.style.border ="2px solid red";
+  }
+}
+
+//validate emails
+export function validateEmailInput(email, errorContainer) {
+  const emailRegEx = /^([a-z0-9_\.\+-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  const validateEmail = emailRegEx.test(email.value);
+  if (validateEmail){ 
+    errorContainer.innerText = "";
+    email.style.border ="2px solid green";
+    return true;
+  } else {
+    errorContainer.innerText = `Please enter a valid email address`;
+    email.style.border ="2px solid red";
+  }
 }
